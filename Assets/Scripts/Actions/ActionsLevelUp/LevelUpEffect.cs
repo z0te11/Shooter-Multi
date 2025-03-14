e0d14d76eadeedaf96e03dd5bea6402254614f36
z0te11 <zotetop@gmail.com>
2025-03-14 14:31:13 +0300

    Game 0.17

--- Assets/Scripts/Actions/ActionsLevelUp/LevelUpEffect.cs
+++ Assets/Scripts/Actions/ActionsLevelUp/LevelUpEffect.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class LevelUpEffect : MonoBehaviour, ILevelUp
 {
     public GameObject effect;
-    public AK.Wwise.Event lvlUpEvent = null;
     public void LevelUp(ExperiancePlayer data, int level)
     {
-        if (lvlUpEvent != null) lvlUpEvent.Post(this.gameObject);
         var newEffect = Instantiate(effect, transform.position, Quaternion.identity);
         Destroy(newEffect, 3f);
         Debug.Log("LevelUpEffect");
